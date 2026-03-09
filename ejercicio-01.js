dd44c5c8e65127d72ca3607ac02cecccab7c1f34
Ruddy <ruddy391@gmail.com>
2026-03-09 21:52:05 +0100

    Realizar ejercicio 1

---
+++ ejercicio-01.js
@@ -0,0 +1,18 @@
+/*### Ejercicio 1: Nombres y datos de entrada
+
+1. Crea una variable `conductor` y asígnale tu nombre.
+2. Muestra por consola el mensaje: `"El nombre del conductor es [nombre]"`.
+3. Pide al usuario que introduzca el nombre del navegante usando `prompt()` y guárdalo en una variable `navegante`.
+4. Muestra por consola: `"El nombre del navegante es [nombre]"`.
+
+**Documentación:** `let` · `console.log()` · `prompt()`  */
+
+let conductor="Ruddy";
+
+console.log(`El nombre del conductor es ${conductor}`);
+
+let navegante=prompt("¿Cómo se llama el navegante?"); 
+/*Aquí prompt hay que verlo desde el navegador en modo inspeccionar*/
+
+
+console.log(`El nombre del navegante es ${navegante}`);
